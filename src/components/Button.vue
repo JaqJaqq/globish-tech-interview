4dffc4f55507a11ae3d9b534f14bc3d277dea2cf
Anurat Likitdachavong <130462497+JaqJaqq@users.noreply.github.com>
2023-11-11 22:29:44 +0700

    feat: add styling QuizCard

---
+++ src/components/Button.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="btn-wrapper">
+      <div class="question-num">
+        <button class="btn-back">กลับ</button>
+      </div>
+      <div class="quiz">
+        <button class="btn-next">ต่อไป</button>
+      </div>
+    </div>
+  </template>
+  <script setup lang="ts">
+  import { ref } from "vue";
+  
+  const questions = ref([
+    {
+      questions: "ถ้าวันนี้เป็นวันหยุด คุณจะ...",
+    },
+  ]);
+  </script>
+  <style scoped>
+  .btn-wrapper {
+    height: 51%;
+    width: 40%;
+    flex-shrink: 0;
+    border-radius: 20px;
+    border: 1px solid #000;
+    background: #fff;
+  }
+  .question-num {
+    display: flex;
+    justify-content: start;
+    align-items: start;
+    padding: 35px 0 0 30px;
+  }
+  img {
+    width: 40px;
+    height: 35px;
+    flex-shrink: 0;
+  }
+  .question {
+    display: flex;
+    justify-content: start;
+    align-items: start;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    color: #000;
+    padding: 30px 15px 0 30px;
+    text-align: left;
+  }
+  #answer-bottons {
+    margin-top: 40px;
+  }
+  .btn {
+    background: #fff;
+    color: #222;
+    font-weight: 500;
+    width: 80%;
+    height: 72px;
+    border: 1px solid #d9d9d9;
+    padding: 10px;
+    margin: 10px 0;
+    text-align: left;
+    border-radius: 20px;
+    cursor: pointer;
+    color: #10153c;
+    font-family: IBM Plex Sans Thai;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    padding-left: 26px;
+  }
+  .btn:hover {
+    background: #f57c4a;
+    border: 1px solid #000;
+  }
+  </style>
--- src/components/QuizCard.vue
+++ src/components/QuizCard.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="wrapper">
-    <div></div>
+    <div class="question-num">
+      <img src="../assets/Q1.png" alt="question number" />
+    </div>
+    <div class="quiz">
+      <h2 class="question">ถ้าวันนี้เป็นวันหยุด คุณจะ...</h2>
+      <div id="answer-bottons">
+        <button class="btn">นอนเล่น พักผ่อน</button>
+        <button class="btn">ดูหนัง ดูซีรี่ส์ เล่นเกม</button>
+        <button class="btn">เรียนรู้อะไรใหม่ ๆ</button>
+      </div>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
@@ -14,14 +24,61 @@ const questions = ref([
 </script>
 <style scoped>
 .wrapper {
-  height: 478px;
-  width: 316px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  height: 51%;
+  width: 40%;
   flex-shrink: 0;
   border-radius: 20px;
   border: 1px solid #000;
   background: #fff;
+}
+.question-num {
+  display: flex;
+  justify-content: start;
+  align-items: start;
+  padding: 35px 0 0 30px;
+}
+img {
+  width: 40px;
+  height: 35px;
+  flex-shrink: 0;
+}
+.question {
+  display: flex;
+  justify-content: start;
+  align-items: start;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  color: #000;
+  padding: 30px 15px 0 30px;
+  text-align: left;
+}
+#answer-bottons {
+  margin-top: 40px;
+}
+.btn {
+  background: #fff;
+  color: #222;
+  font-weight: 500;
+  width: 80%;
+  height: 72px;
+  border: 1px solid #d9d9d9;
+  padding: 10px;
+  margin: 10px 0;
+  text-align: left;
+  border-radius: 20px;
+  cursor: pointer;
+  color: #10153c;
+  font-family: IBM Plex Sans Thai;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  padding-left: 26px;
+}
+.btn:hover {
+  background: #f57c4a;
+  border: 1px solid #000;
 }
 </style>
